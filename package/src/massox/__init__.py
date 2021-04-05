a1620cb4323f2ac22b437af70882b760c935a6df
nicolamassarenti <nicola.massarenti@gmail.com>
2021-04-05 18:15:02 +0200

    Added loading of configs

---
+++ package/src/massox/__init__.py
@@ -0,0 +1,16 @@
+from box import Box
+
+config = None
+
+def set_config(path: str):
+    try:
+        with open(path) as f:
+            config = yaml.load(f, Loader=yaml.SafeLoader)
+
+        config = Box(config)
+    except FileNotFoundError as e:
+        print(str(e))
+
+
+if config is None:
+    print("Massox configs are None - remember to set the value before to use this package!")
