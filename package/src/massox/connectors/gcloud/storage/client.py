91f3e30dda4fa388238e89d1fe5667e61b7a7753
nicolamassarenti <nicola.massarenti@gmail.com>
2021-04-28 22:02:24 +0200

    Added gcloud storage handler

--- package/src/massox/connectors/gcloud/storage/client.py
+++ package/src/massox/connectors/gcloud/storage/client.py
@@ -1,50 +1,58 @@
 from google.cloud import storage
 
 
-class StorageConnector:
+class GCloudStorageConnector:
     def __init__(self):
+        self._name = "GCloudStorageConnector"
         self._client = storage.Client()
 
-    def upload_from_string(self, data: bytes, bucket_name: str, destination_blob_name: str):
-        self._client. \
-            bucket(bucket_name=bucket_name). \
-            blob(blob_name=destination_blob_name).\
-            upload_from_string(data=data)
+    def __str__(self):
+        return self._name
+
+    def upload_from_string(
+        self, data: bytes, bucket_name: str, destination_blob_name: str
+    ):
+        self._client.bucket(bucket_name=bucket_name).blob(
+            blob_name=destination_blob_name
+        ).upload_from_string(data=data)
 
     def download_as_string(self, bucket_name: str, source_blob_name: str):
-        return self._client. \
-            bucket(bucket_name=bucket_name). \
-            blob(blob_name=source_blob_name). \
-            download_as_string()
+        return (
+            self._client.bucket(bucket_name=bucket_name)
+            .blob(blob_name=source_blob_name)
+            .download_as_string()
+        )
 
     def exists(self, bucket_name: str, source_blob_name: str):
-        return self._client. \
-            bucket(bucket_name=bucket_name). \
-            blob(blob_name=source_blob_name). \
-            exists()
+        return (
+            self._client.bucket(bucket_name=bucket_name)
+            .blob(blob_name=source_blob_name)
+            .exists()
+        )
 
     def list_blobs(self, bucket_name: str, prefix: str = None, delimiter: str = None):
-        return self._client.\
-            bucket(bucket_name=bucket_name).\
-            list_blobs(prefix=prefix, delimiter=delimiter)
+        return self._client.bucket(bucket_name=bucket_name).list_blobs(
+            prefix=prefix, delimiter=delimiter
+        )
 
-    def copy(self, source_bucket_name: str, source_blob_name: str, dest_bucket_name: str, dest_blob_name: str):
+    def copy(
+        self,
+        source_bucket_name: str,
+        source_blob_name: str,
+        dest_bucket_name: str,
+        dest_blob_name: str,
+    ):
         source_bucket = self._client.bucket(bucket_name=source_bucket_name)
         source_blob = source_bucket.blob(blob_name=source_blob_name)
 
         dest_bucket = self._client.bucket(bucket_name=dest_bucket_name)
 
         return source_bucket.copy_blob(
-            blob=source_blob,
-            destination_bucket=dest_bucket,
-            new_name=dest_blob_name
+            blob=source_blob, destination_bucket=dest_bucket, new_name=dest_blob_name
         )
 
     def delete(self, bucket_name: str, blob_name: str):
-         self._client.\
-            bucket(bucket_name=bucket_name).\
-            blob(blob_name=blob_name).\
-            delete()
+        self._client.bucket(bucket_name=bucket_name).blob(blob_name=blob_name).delete()
 
 
-client = StorageConnector()
+gcloud_storage_client = GCloudStorageConnector()
---
+++ package/src/massox/handlers/gcloud/storage/handler.py
@@ -0,0 +1,88 @@
+import json
+
+
+from massox.connectors.gcloud.storage.client import gcloud_storage_client
+from massox.types.gcloud.storage.location import StorageLocation, StorageLocationBuilder
+from massox.types.data.extensions import DataExtensions
+
+
+class GCloudStorageHandler:
+    def __init__(self):
+        self._name = "GCloudStorageHandler"
+
+    def __str__(self):
+        return self._name
+
+    def get(self, location: StorageLocation = None):
+        data = self._client.download_as_string(
+            bucket_name=location.get_bucket(), source_blob_name=location.get_blob_name()
+        )
+
+        if location.filename.endswith(DataExtensions.NUMPY.value):
+            return np.frombuffer(data, dtype=np.float64)
+
+        elif location.filename.endswith(DataExtensions.JSON.value):
+            return json.loads(data)
+        else:
+            NotImplementedError("File extension not managed for saving")
+            return
+
+    def save(self, obj, location: StorageLocation = None):
+        data = None
+        if location.filename.endswith(DataExtensions.NUMPY.value):
+            obj = obj.astype("float64")
+            data = obj.tostring()
+
+        elif location.filename.endswith(DataExtensions.JSON.value):
+            data = json.dumps(obj)
+        else:
+            NotImplementedError("File extension not managed for saving")
+            return
+
+        self._client.upload_from_string(
+            data=data,
+            bucket_name=location.get_bucket(),
+            destination_blob_name=location.get_blob_name(),
+        )
+
+    def exists(self, location: StorageLocation = None):
+        return self._client.exists(
+            bucket_name=location.get_bucket(), source_blob_name=location.get_blob_name()
+        )
+
+    def get_list_content(self, location: StorageLocation = None) -> [StorageLocation]:
+        blobs = self._client.list_blobs(
+            bucket_name=location.get_bucket(), prefix=location.get_folders()
+        )
+
+        location_list = []
+        for blob in blobs:
+            if blob.name == location.get_folders():
+                # blob is the folder, not a file
+                continue
+            blob_name = os.path.join(location.get_folders(), blob.name.split("/")[-1])
+            base_location = (
+                StorageLocationBuilder()
+                .set_prefix(prefix=location.get_prefix())
+                .set_bucket(bucket=location.get_bucket())
+                .set_blob_name(blob_name=blob_name)
+            )
+            location_list.append(base_location)
+
+        return location_list
+
+    def move(
+        self,
+        source_location: StorageLocation = None,
+        dest_location: StorageLocation = None,
+    ):
+        self._client.copy(
+            source_bucket_name=source_location.get_bucket(),
+            source_blob_name=source_location.get_blob_name(),
+            dest_bucket_name=dest_location.get_bucket(),
+            dest_blob_name=dest_location.get_blob_name(),
+        )
+        return self._client.delete(
+            bucket_name=source_location.get_bucket(),
+            blob_name=source_location.get_blob_name(),
+        )
