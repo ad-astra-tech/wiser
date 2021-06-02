bbbbb4b955d4078c6db2b3b91ecb60f2f0e24609
nicolamassarenti <nicola.massarenti@gmail.com>
2021-06-02 12:11:32 +0200

    Updated StorageLocation class property retrieval

--- package/src/massox/handlers/gcloud/storage/handler.py
+++ package/src/massox/handlers/gcloud/storage/handler.py
@@ -1,5 +1,6 @@
 import json
 
+from pathlib import Path
 
 from massox.connectors.gcloud.storage.client import gcloud_storage_client
 from massox.types.gcloud.storage.location import StorageLocation, StorageLocationBuilder
@@ -16,7 +17,7 @@ class GCloudStorageHandler:
 
     def get(self, location: StorageLocation = None):
         data = self._client.download_as_string(
-            bucket_name=location.get_bucket(), source_blob_name=location.get_blob_name()
+            bucket_name=location.bucket, source_blob_name=location.blob_name
         )
 
         if location.filename.endswith(DataExtension.NUMPY.value):
@@ -25,7 +26,7 @@ class GCloudStorageHandler:
         elif location.filename.endswith(DataExtension.JSON.value):
             return json.loads(data)
         else:
-            NotImplementedError("File extension not managed for saving")
+            NotImplementedError("File extension not managed")
             return
 
     def save(self, obj, location: StorageLocation = None):
@@ -37,35 +38,35 @@ class GCloudStorageHandler:
         elif location.filename.endswith(DataExtension.JSON.value):
             data = json.dumps(obj)
         else:
-            NotImplementedError("File extension not managed for saving")
+            NotImplementedError("File extension not managed")
             return
 
         self._client.upload_from_string(
             data=data,
-            bucket_name=location.get_bucket(),
-            destination_blob_name=location.get_blob_name(),
+            bucket_name=location.bucket,
+            destination_blob_name=location.blob_name,
         )
 
     def exists(self, location: StorageLocation = None):
         return self._client.exists(
-            bucket_name=location.get_bucket(), source_blob_name=location.get_blob_name()
+            bucket_name=location.bucket, source_blob_name=location.blob_name
         )
 
     def get_list_content(self, location: StorageLocation = None) -> [StorageLocation]:
         blobs = self._client.list_blobs(
-            bucket_name=location.get_bucket(), prefix=location.get_folders()
+            bucket_name=location.bucket, prefix=location.folders
         )
 
         location_list = []
         for blob in blobs:
-            if blob.name == location.get_folders():
+            if blob.name == location.folders:
                 # blob is the folder, not a file
                 continue
-            blob_name = os.path.join(location.get_folders(), blob.name.split("/")[-1])
+            blob_name = str(Path(location.folders).joinpath(blob.name.split("/")[-1]))
             base_location = (
                 StorageLocationBuilder()
-                .set_prefix(prefix=location.get_prefix())
-                .set_bucket(bucket=location.get_bucket())
+                .set_prefix(prefix=location.prefix)
+                .set_bucket(bucket=location.bucket)
                 .set_blob_name(blob_name=blob_name)
             )
             location_list.append(base_location)
@@ -78,12 +79,12 @@ class GCloudStorageHandler:
         dest_location: StorageLocation = None,
     ):
         self._client.copy(
-            source_bucket_name=source_location.get_bucket(),
-            source_blob_name=source_location.get_blob_name(),
-            dest_bucket_name=dest_location.get_bucket(),
-            dest_blob_name=dest_location.get_blob_name(),
+            source_bucket_name=source_location.bucket,
+            source_blob_name=source_location.blob_name,
+            dest_bucket_name=dest_location.bucket,
+            dest_blob_name=dest_location.blob_name,
         )
         return self._client.delete(
-            bucket_name=source_location.get_bucket(),
-            blob_name=source_location.get_blob_name(),
+            bucket_name=source_location.bucket,
+            blob_name=source_location.blob_name,
         )
