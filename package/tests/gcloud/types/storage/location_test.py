6a91074db70e7b97055937437b0aa5e2b3f23389
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-08-28 16:53:02 +0200

    Added storage location tests

--- package/tests/gcloud/types/storage/location_test.py
+++ package/tests/gcloud/types/storage/location_test.py
@@ -3,12 +3,12 @@ import pytest
 
 def test_location_builder_no_bucket_raises_valueerror():
     """
-    GIVEN LocationBuilder
+    GIVEN StorageLocationBuilder
     WHEN  the bucket is not set
     THEN a value error is raised
     """
 
-    from massox.gcloud.types.storage import StorageLocationBuilder
+    from massox.gcloud.types.storage.location import StorageLocationBuilder
 
     with pytest.raises(ValueError):
         StorageLocationBuilder().set_blob_name(
@@ -18,12 +18,12 @@ def test_location_builder_no_bucket_raises_valueerror():
 
 def test_location_builder_no_blobname_raises_valueerror():
     """
-    GIVEN LocationBuilder
+    GIVEN StorageLocationBuilder
     WHEN  the blob name is not set
     THEN a value error is raised
     """
 
-    from massox.gcloud.types.storage import StorageLocationBuilder
+    from massox.gcloud.types.storage.location import StorageLocationBuilder
 
     with pytest.raises(ValueError):
         StorageLocationBuilder().set_bucket(bucket="bucket_name").build()
@@ -33,10 +33,10 @@ def test_location_builder_returns_storage_location():
     """
     GIVEN LocationBuilder
     WHEN  the blob name and the bucket are set
-    THEN a storage location object is returned
+    THEN a fully featured storage location object is returned
     """
 
-    from massox.gcloud.types.storage import StorageLocationBuilder
+    from massox.gcloud.types.storage.location import StorageLocationBuilder
 
     prefix = "gs://"
     bucket = "bucket_name"
@@ -57,4 +57,4 @@ def test_location_builder_returns_storage_location():
     assert storage_location.folders == folders
     assert storage_location.filename == filename
     assert storage_location.blob_name == blob_name
-    assert storage_location.complete_path == complete_path
+    assert storage_location.complete_path() == complete_path
