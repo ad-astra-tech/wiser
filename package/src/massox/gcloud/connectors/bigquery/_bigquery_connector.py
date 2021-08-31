042df423fa10769847efa5be154848749a503caa
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-08-31 14:26:57 +0200

    Updated bigquery connector

--- package/src/massox/gcloud/connectors/bigquery/_bigquery_connector.py
+++ package/src/massox/gcloud/connectors/bigquery/_bigquery_connector.py
@@ -1,29 +1,29 @@
 from typing import List, Dict, Optional, Any
 
 from google.cloud import bigquery
-from google.cloud.bigquery import QueryJobConfig
+
+from massox.gcloud.types.bigquery.table import BigQueryTableReference
+from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfig
 
 
 class BigQueryConnector:
     @staticmethod
     def query(
-        query: str, job_config: QueryJobConfig = None
+        query: str, job_config: BigQueryQueryJobConfig = None
     ) -> Optional[bigquery.job.QueryJob]:
         if job_config is not None:
-            return bigquery.Client().query(query, job_config=job_config)
+            return bigquery.Client().query(query, job_config=job_config.job_config)
         else:
             bigquery.Client().query(query)
 
     @staticmethod
-    def insert(table: BigQueryTable, rows: List[Dict[str, Any]]):
+    def insert(table: BigQueryTableReference, rows: List[Dict[str, Any]]):
 
         # Insert with checks
-        errors = bigquery.Client().insert_rows_json(table.table_ref, rows)
+        errors = bigquery.Client().insert_rows_json(table.reference, rows)
         if errors:
             raise ValueError(
                 "{} rows had errors and could not be inserted: {}".format(
                     len(errors), errors
                 )
             )
-        else:
-            logger.info("Inserted.")
