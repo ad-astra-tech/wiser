905a12544a18511aa9a17d305cc6d58c491ff17c
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-08-31 09:12:17 +0200

    WIP

--- package/src/massox/gcloud/types/bigquery/dataset.py
+++ package/src/massox/gcloud/types/bigquery/dataset.py
@@ -11,11 +11,11 @@ class BigQueryDatasetReference:
         self._dataset = DatasetReference(project=project_id, dataset_id=dataset_id)
 
     @property
-    def dataset_id(self):
+    def dataset_id(self) -> str:
         return self._dataset_id
 
     @property
-    def project_id(self):
+    def project_id(self) -> str:
         return self._project_id
 
     def to_api_repr(self) -> Dict[str, str]:
@@ -27,17 +27,17 @@ class BigQueryDatasetReferenceBuilder:
         self._project_id = None
         self._dataset_id = None
 
-    def set_project_id(self, project_id: str):
+    def set_project_id(self, project_id: str) -> None:
         self._project_id = project_id
 
-    def set_dataset_id(self, dataset_id: str):
+    def set_dataset_id(self, dataset_id: str) -> None:
         self._dataset_id = dataset_id
 
     def build(self) -> BigQueryDatasetReference:
         if self._dataset_id is None:
-            raise ValueError("Dataset id not set")
+            raise ValueError("Dataset id is None")
         if self._project_id is None:
-            raise ValueError("Project id not set")
+            raise ValueError("Project id is None")
 
         return BigQueryDatasetReference(
             dataset_id=self._dataset_id, project_id=self._project_id
--- package/src/massox/gcloud/types/bigquery/table.py
+++ package/src/massox/gcloud/types/bigquery/table.py
@@ -1,4 +1,42 @@
+from typing import Dict, Any
+
+from massox.gcloud.types.bigquery.dataset import BigQueryDatasetReference
 
 
 class BigQueryTable:
-    pass
+    def __init__(self, dataset_reference: BigQueryDatasetReference, table_id: str):
+        self._dataset_reference = dataset_reference
+        self._table_id = table_id
+
+    @property
+    def dataset_reference(self):
+        return self._dataset_reference
+
+    @property
+    def table_id(self):
+        return self._table_id
+
+    def to_api_repr(self) -> Dict[str, Any]:
+        return self._dataset_reference.to_api_repr()
+
+
+class BigQueryTableBuilder:
+    def __init__(self):
+        self._dataset_reference = None
+        self._table_id = None
+
+    def set_dataset_reference(self, dataset_reference: BigQueryDatasetReference):
+        self._dataset_reference = dataset_reference
+
+    def set_table_id(self, table_id: str):
+        self._table_id = table_id
+
+    def build(self) -> BigQueryTable:
+        if self._dataset_reference is None:
+            raise ValueError("Dataset reference is None")
+        if self._table_id is None:
+            raise ValueError("Table id is None")
+
+        return BigQueryTable(
+            dataset_reference=self._dataset_reference, table_id=self._table_id
+        )
