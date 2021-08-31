d7dcd9a7b5ec3aef8f3e76fd44bb82f9502e281e
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-08-31 15:46:11 +0200

    Removed useless file

--- package/src/massox/gcloud/types/bigquery/write_disposition.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-from google.cloud.bigquery.job import WriteDisposition
-
-
-class BigQueryWriteDisposition(Enum):
-    WRITE_TRUNCATE: WriteDisposition.WRITE_TRUNCATE
-    WRITE_APPEND: WriteDisposition.WRITE_APPEND
-    WRITE_EMPTY: WriteDisposition.WRITE_EMPTY
