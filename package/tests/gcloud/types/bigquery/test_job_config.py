b5b5860c4152fd3e4c208b70d4a0e0c5c6f5b08a
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-09-02 14:47:32 +0200

    Converted to unittest

--- package/tests/gcloud/types/bigquery/test_job_config.py
+++ package/tests/gcloud/types/bigquery/test_job_config.py
@@ -1,100 +1,102 @@
-import pytest
+import unittest
+
+class BigQueryJobConfigTest(unittest.TestCase):
 
 
-def test_query_job_config_builder_no_write_disposition_raises_value_error():
-    """
-    GIVEN BigQueryQueryJobConfigBuilder
-    WHEN  the write disposition is not set
-    THEN a value error is raised
-    """
+    def test_query_job_config_builder_no_write_disposition_raises_value_error(self):
+        """
+        GIVEN BigQueryQueryJobConfigBuilder
+        WHEN  the write disposition is not set
+        THEN a value error is raised
+        """
 
-    from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
-    from massox.gcloud.types.bigquery.table import BigQueryTableReferenceBuilder
-    from massox.gcloud.types.bigquery.dataset import BigQueryDatasetReferenceBuilder
+        from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
+        from massox.gcloud.types.bigquery.table import BigQueryTableReferenceBuilder
+        from massox.gcloud.types.bigquery.dataset import BigQueryDatasetReferenceBuilder
 
-    dataset_id = "fake_dataset_id"
-    project_id = "fake_project_id"
-    table_id = "fake_table_id"
+        dataset_id = "fake_dataset_id"
+        project_id = "fake_project_id"
+        table_id = "fake_table_id"
 
-    dataset_reference = (
-        BigQueryDatasetReferenceBuilder()
-        .set_dataset_id(dataset_id=dataset_id)
-        .set_project_id(project_id=project_id)
-        .build()
-    )
+        dataset_reference = (
+            BigQueryDatasetReferenceBuilder()
+            .set_dataset_id(dataset_id=dataset_id)
+            .set_project_id(project_id=project_id)
+            .build()
+        )
 
-    table_reference = (
-        BigQueryTableReferenceBuilder()
-        .set_dataset_reference(dataset_reference=dataset_reference)
-        .set_table_id(table_id=table_id)
-        .build()
-    )
+        table_reference = (
+            BigQueryTableReferenceBuilder()
+            .set_dataset_reference(dataset_reference=dataset_reference)
+            .set_table_id(table_id=table_id)
+            .build()
+        )
 
-    with pytest.raises(ValueError):
-        BigQueryQueryJobConfigBuilder().set_destination(
-            destination=table_reference
-        ).build()
-
-
-def test_query_job_config_builder_no_destination_raises_value_error():
-    """
-    GIVEN BigQueryQueryJobConfigBuilder
-    WHEN  the write disposition is not set
-    THEN a value error is raised
-    """
-
-    from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
-    from google.cloud.bigquery.job import WriteDisposition
-
-    with pytest.raises(ValueError):
-        BigQueryQueryJobConfigBuilder().set_write_disposition(
-            write_disposition=WriteDisposition.WRITE_TRUNCATE
-        ).build()
+        with self.assertRaises(ValueError):
+            BigQueryQueryJobConfigBuilder().set_destination(
+                destination=table_reference
+            ).build()
 
 
-def test_query_job_config_builder_returns_query_job_config():
-    """
-    GIVEN BigQueryQueryJobConfigBuilder
-    WHEN  the write disposition  and the destination are set
-    THEN BigQueryJobConfig is returned
-    """
+    def test_query_job_config_builder_no_destination_raises_value_error(self):
+        """
+        GIVEN BigQueryQueryJobConfigBuilder
+        WHEN  the write disposition is not set
+        THEN a value error is raised
+        """
 
-    from google.cloud.bigquery.job import WriteDisposition
-    from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
-    from massox.gcloud.types.bigquery.table import BigQueryTableReferenceBuilder
-    from massox.gcloud.types.bigquery.dataset import BigQueryDatasetReferenceBuilder
-    from google.cloud.bigquery.job import QueryJobConfig
+        from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
+        from google.cloud.bigquery.job import WriteDisposition
 
-    dataset_id = "fake_dataset_id"
-    project_id = "fake_project_id"
-    table_id = "fake_table_id"
+        with self.assertRaises(ValueError):
+            BigQueryQueryJobConfigBuilder().set_write_disposition(
+                write_disposition=WriteDisposition.WRITE_TRUNCATE
+            ).build()
 
-    dataset_reference = (
-        BigQueryDatasetReferenceBuilder()
-        .set_dataset_id(dataset_id=dataset_id)
-        .set_project_id(project_id=project_id)
-        .build()
-    )
-    table_reference = (
-        BigQueryTableReferenceBuilder()
-        .set_dataset_reference(dataset_reference=dataset_reference)
-        .set_table_id(table_id=table_id)
-        .build()
-    )
-    write_disposition = WriteDisposition.WRITE_TRUNCATE
 
-    job_config = (
-        BigQueryQueryJobConfigBuilder()
-        .set_write_disposition(write_disposition=write_disposition)
-        .set_destination(destination=table_reference)
-        .build()
-    )
+    def test_query_job_config_builder_returns_query_job_config(self):
+        """
+        GIVEN BigQueryQueryJobConfigBuilder
+        WHEN  the write disposition  and the destination are set
+        THEN BigQueryJobConfig is returned
+        """
 
-    assert job_config.write_disposition == write_disposition
-    assert job_config.destination == table_reference
-    assert (
-        job_config.job_config.to_api_repr()
-        == QueryJobConfig(
-            destination=table_reference, write_disposition=write_disposition
-        ).to_api_repr()
-    )
+        from google.cloud.bigquery.job import WriteDisposition
+        from massox.gcloud.types.bigquery.job_config import BigQueryQueryJobConfigBuilder
+        from massox.gcloud.types.bigquery.table import BigQueryTableReferenceBuilder
+        from massox.gcloud.types.bigquery.dataset import BigQueryDatasetReferenceBuilder
+        from google.cloud.bigquery.job import QueryJobConfig
+
+        dataset_id = "fake_dataset_id"
+        project_id = "fake_project_id"
+        table_id = "fake_table_id"
+
+        dataset_reference = (
+            BigQueryDatasetReferenceBuilder()
+            .set_dataset_id(dataset_id=dataset_id)
+            .set_project_id(project_id=project_id)
+            .build()
+        )
+        table_reference = (
+            BigQueryTableReferenceBuilder()
+            .set_dataset_reference(dataset_reference=dataset_reference)
+            .set_table_id(table_id=table_id)
+            .build()
+        )
+        write_disposition = WriteDisposition.WRITE_TRUNCATE
+
+        job_config = (
+            BigQueryQueryJobConfigBuilder()
+            .set_write_disposition(write_disposition=write_disposition)
+            .set_destination(destination=table_reference)
+            .build()
+        )
+
+        self.assertEqual(job_config.write_disposition, write_disposition)
+        self.assertEqual(job_config.destination, table_reference)
+        self.assertEqual(
+            job_config.job_config.to_api_repr(),
+            QueryJobConfig(
+                destination=table_reference, write_disposition=write_disposition
+            ).to_api_repr()
+        )
