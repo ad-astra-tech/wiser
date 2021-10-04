2bc91c2c55ecbae1c69043fa1c0497d45bfbaaa3
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-10-04 17:20:25 +0200

    Removed unused functions

--- package/tests/gcloud/connectors/storage/test_storage_connector.py
+++ package/tests/gcloud/connectors/storage/test_storage_connector.py
@@ -2,38 +2,13 @@ import unittest
 from unittest.mock import patch
 
 import google.cloud.storage
-import mock
-import requests
-
-from six.moves import http_client
 
 PROJECT = "PROJECT"
 BUCKET_NAME = "BUCKET"
 BLOB_NAME = "BLOB"
 
 
-def _make_response(status=http_client.OK, content=b"", headers={}):
-    response = requests.Response()
-    response.status_code = status
-    response._content = content
-    response.headers = headers
-    response.request = requests.Request()
-    return response
-
-
 class StorageConnectorTest(unittest.TestCase):
-    @staticmethod
-    def _make_credentials():
-        import google.auth.credentials
-
-        return mock.Mock(spec=google.auth.credentials.Credentials)
-
-    @staticmethod
-    def _get_client(credentials):
-        from google.cloud.storage.client import Client
-
-        return Client(credentials=credentials)
-
     @staticmethod
     def _get_bucket(client: google.cloud.storage.Client, name: str):
         from google.cloud.storage.bucket import Bucket
@@ -45,12 +20,6 @@ class StorageConnectorTest(unittest.TestCase):
         from google.cloud.storage.blob import Blob
 
         return Blob(bucket=bucket, name=blob_name)
-
-    def test(self):
-        credentials = self._make_credentials()
-        client = self._get_client(credentials=credentials)
-        bucket = self._get_bucket(client, name=BUCKET_NAME)
-        blob = self._get_blob(blob_name=BLOB_NAME, bucket=bucket)
 
     @patch("google.cloud.storage.Client")
     @patch("google.cloud.storage.Bucket")
