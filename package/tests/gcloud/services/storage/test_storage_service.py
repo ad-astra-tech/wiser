049c4553c61bee5ff3e0a84d7ef12708b3655241
Nicola <nicola.massarenti@gmail.com>
2021-11-07 00:58:22 +0100

    Added tests for storage service

--- package/tests/gcloud/services/storage/test_storage_service.py
+++ package/tests/gcloud/services/storage/test_storage_service.py
@@ -5,7 +5,7 @@ BUCKET = "bucket"
 BLOB_NAME = "path/to/blob"
 
 
-class StorageHandlerTest(unittest.TestCase):
+class StorageServiceTest(unittest.TestCase):
     def test_get_with_location_no_blob_name_raises_value_error(self):
         """
         GIVEN   a location with no blob_name (i.e. a folder)
@@ -21,9 +21,8 @@ class StorageHandlerTest(unittest.TestCase):
             Storage.get(location=location)
 
 
-@patch("massox.gcloud.connectors.storage.StorageConnector.download_as_string")
-class StorageHandlerTest(unittest.TestCase):
-    def test_get_stromg(self, storage_connector_mock):
+    @patch("massox.gcloud.connectors.storage.StorageConnector.download_as_string")
+    def test_get_string(self, storage_connector_mock):
         """
         GIVEN   a valid location
         WHEN    the blob refers to a string
@@ -31,7 +30,6 @@ class StorageHandlerTest(unittest.TestCase):
         """
         from massox.gcloud.services.storage import Storage
         from massox.gcloud.types.storage.location import StorageLocationBuilder
-        import numpy as np
 
         location = (
             StorageLocationBuilder()
@@ -42,5 +40,31 @@ class StorageHandlerTest(unittest.TestCase):
 
         data = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
         storage_connector_mock.return_value = data
+
+        self.assertEqual(Storage.get(location=location), data)
+
+    @patch("massox.gcloud.connectors.storage.StorageConnector.download_as_string")
+    def test_get_json(self, storage_connector_mock):
+        """
+        GIVEN   a valid location
+        WHEN    the blob refers to a json file
+        THEN    the expected json is returned
+        """
+        import json
+        from massox.gcloud.services.storage import Storage
+        from massox.gcloud.types.storage.location import StorageLocationBuilder
+
+        location = (
+            StorageLocationBuilder()
+            .set_bucket(bucket=BUCKET)
+            .set_blob_name(blob_name="path/to/data.json")
+            .build()
+        )
+
+        data = {
+            "1": "a",
+            "2": "b"
+        }
+        storage_connector_mock.return_value = json.dumps(obj=data, sort_keys=True, indent=4, ensure_ascii=False)
 
         self.assertEqual(Storage.get(location=location), data)
