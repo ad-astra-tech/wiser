4678c8c3d1a20f2ec1663d3fc66a293357d50007
Nicola <nicola.massarenti@gmail.com>
2021-11-07 00:51:09 +0100

    renamed module

--- package/tests/gcloud/services/storage/test_storage_service.py
+++ package/tests/gcloud/services/storage/test_storage_service.py
@@ -12,37 +12,35 @@ class StorageHandlerTest(unittest.TestCase):
         WHEN    if invoked `get()` method
         THEN    value error is raised
         """
-        from massox.gcloud.handlers.storage import StorageHandler
+        from massox.gcloud.services.storage import Storage
         from massox.gcloud.types.storage.location import StorageLocationBuilder
 
         location = StorageLocationBuilder().set_bucket(bucket=BUCKET).build()
 
         with self.assertRaises(ValueError):
-            StorageHandler.get(location=location)
+            Storage.get(location=location)
 
 
 @patch("massox.gcloud.connectors.storage.StorageConnector.download_as_string")
 class StorageHandlerTest(unittest.TestCase):
-    def test_get_numpy_array(self, storage_connector_mock):
+    def test_get_stromg(self, storage_connector_mock):
         """
         GIVEN   a valid location
-        WHEN    the blob refers to a numpy array
-        THEN    a numpy array is returned
+        WHEN    the blob refers to a string
+        THEN    the expected string is returned
         """
-        from massox.gcloud.handlers.storage import StorageHandler
+        from massox.gcloud.services.storage import Storage
         from massox.gcloud.types.storage.location import StorageLocationBuilder
         import numpy as np
 
         location = (
             StorageLocationBuilder()
             .set_bucket(bucket=BUCKET)
-            .set_blob_name(blob_name="path/to/file.npy")
+            .set_blob_name(blob_name="path/to/text.txt")
             .build()
         )
 
-        data = np.array([[1, 2, 3, 4], [1, 2, 3, 4]])
-        bytessss = data.tobytes()
-        datass = bytessss.decode(encoding="utf-8")
-        storage_connector_mock.return_value = datass
+        data = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+        storage_connector_mock.return_value = data
 
-        self.assertEqual(StorageHandler.get(location=location), data)
+        self.assertEqual(Storage.get(location=location), data)
