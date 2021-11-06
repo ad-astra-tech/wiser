b911bd36f4a71cb584e2a8e822a473bd7315602b
Nicola <nicola.massarenti@gmail.com>
2021-11-07 00:02:25 +0100

    Added list example

--- package/examples/storage/list.py
+++ package/examples/storage/list.py
@@ -5,5 +5,19 @@ from massox.gcloud.types.storage.location import StorageLocationBuilder
 
 BUCKET_NAME = os.getenv("BUCKET_NAME")
 
+# Listing content of a bucket
 location = StorageLocationBuilder().set_bucket(bucket=BUCKET_NAME).build()
 blobs = StorageHandler.get_list_content(location=location)
+for blob in blobs:
+    print(blob.complete_path())
+
+# Listing content of a specific folder
+location = (
+    StorageLocationBuilder()
+    .set_bucket(bucket=BUCKET_NAME)
+    .set_blob_name(blob_name="folder_a")
+    .build()
+)
+blobs = StorageHandler.get_list_content(location=location)
+for blob in blobs:
+    print(blob.complete_path())
