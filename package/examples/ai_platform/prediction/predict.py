2e0966d4823ee161677e8f3e3e673139af8953ce
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-11-15 08:44:17 +0100

    Added ai platform prediction example

---
+++ package/examples/ai_platform/prediction/predict.py
@@ -0,0 +1,21 @@
+import os
+from wiser.gcloud.services.ai_platform import AIPlatform
+
+PROJECT_ID = os.getenv("PROJECT_ID")
+ENDPOINT = os.getenv("ENDPOINT")
+MODEL_NAME = os.getenv("MODEL_NAME")
+
+# Body depends on the signature of your model
+body = dict(
+    instances=[
+        dict(input="This is the first sentence..."),
+        dict(input="... and this is the second sentence")
+    ]
+)
+
+predictions = AIPlatform.prediction.predict(
+    project_id=PROJECT_ID,
+    endpoint=ENDPOINT,
+    model_name=MODEL_NAME,
+    body=body
+)
