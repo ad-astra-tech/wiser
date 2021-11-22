347b28a1bd0a42546d29ad2eeb544de297601290
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-11-22 18:11:20 +0100

    Added 'add' examples

---
+++ package/examples/firestore/add.py
@@ -0,0 +1,23 @@
+from wiser.gcloud.services import Firestore
+from wiser.gcloud.types.firestore import (
+    FirestoreDocumentBuilder,
+    FirestoreCollectionBuilder,
+)
+
+data = {"key_1": "value_1", "key_2": "value_2"}
+collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+document = FirestoreDocumentBuilder().set_data(data=data).build()
+Firestore.add(collection=collection, document=document)
+
+data = {
+    "key_1": "value_1",
+    "key_2": "value_2",
+    "key_3": {"key_4": "value_4", "key_5": "value_6"},
+}
+collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+document = FirestoreDocumentBuilder()
+for key, value in data.items():
+    document.add_property(key=key, value=value)
+
+document = document.build()
+Firestore.add(collection=collection, document=document)
