3e98e9e02ddb2e41263bc22249a320a877fc98c3
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-11-24 07:25:03 +0100

    Added imports

--- package/src/wiser/gcloud/types/firestore/__init__.py
+++ package/src/wiser/gcloud/types/firestore/__init__.py
@@ -1,4 +1,15 @@
 from wiser.gcloud.types.firestore.document import FirestoreDocumentBuilder
 from wiser.gcloud.types.firestore.collection import FirestoreCollectionBuilder
+from wiser.gcloud.types.firestore.query import (
+    FirestoreQueryBuilder,
+    FirestoreQueryCondition,
+    FirestoreQueryDirection,
+)
 
-__all__ = ["FirestoreDocumentBuilder", "FirestoreCollectionBuilder"]
+__all__ = [
+    "FirestoreDocumentBuilder",
+    "FirestoreCollectionBuilder",
+    "FirestoreQueryBuilder",
+    "FirestoreQueryCondition",
+    "FirestoreQueryDirection",
+]
