74d8ed00b0514684794d3ef73fe369736c4494ae
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-11-24 07:24:07 +0100

    modified document, formatting with black

--- package/examples/firestore/add.py
+++ package/examples/firestore/add.py
@@ -4,8 +4,14 @@ from wiser.gcloud.types.firestore import (
     FirestoreCollectionBuilder,
 )
 
+COLLECTION_NAME = "collection_name"
+
 data = {"key_1": "value_1", "key_2": "value_2"}
-collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+collection = (
+    FirestoreCollectionBuilder()
+    .set_collection_name(collection_name=COLLECTION_NAME)
+    .build()
+)
 document = (
     FirestoreDocumentBuilder().set_id(id="my_custom_id").set_data(data=data).build()
 )
@@ -13,10 +19,14 @@ Firestore.add(collection=collection, document=document)
 
 data = {
     "key_1": "value_1",
-    "key_2": "value_2",
+    "key_2": "xxxx",
     "key_3": {"key_4": "value_4", "key_5": "value_6"},
 }
-collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+collection = (
+    FirestoreCollectionBuilder()
+    .set_collection_name(collection_name=COLLECTION_NAME)
+    .build()
+)
 document = FirestoreDocumentBuilder()
 for key, value in data.items():
     document.add_property(key=key, value=value)
