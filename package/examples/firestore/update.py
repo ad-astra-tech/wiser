4ddd39e2a4f0e3f7d7c3696c765b4d585a65eaa4
Nicola Massarenti <nicola.massarenti@noovle.com>
2021-11-24 07:24:17 +0100

    renamed parameters

--- package/examples/firestore/update.py
+++ package/examples/firestore/update.py
@@ -7,13 +7,21 @@ from wiser.gcloud.types.firestore import (
 # ...Supposing that document with id "my_custom_id" already exists
 
 data = {"key_1": "value_1", "key_2": "value_4"}
-collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+collection = (
+    FirestoreCollectionBuilder()
+    .set_collection_name(collection_name="collection_path")
+    .build()
+)
 document = (
     FirestoreDocumentBuilder().set_id(id="my_custom_id").set_data(data=data).build()
 )
 Firestore.update(collection=collection, document=document)
 data = {"key_1": "foo", "key_2": "boo"}
-collection = FirestoreCollectionBuilder().set_path(path="collection_path").build()
+collection = (
+    FirestoreCollectionBuilder()
+    .set_collection_name(collection_name="collection_path")
+    .build()
+)
 document = FirestoreDocumentBuilder()
 for key, value in data.items():
     document.add_property(key=key, value=value)
