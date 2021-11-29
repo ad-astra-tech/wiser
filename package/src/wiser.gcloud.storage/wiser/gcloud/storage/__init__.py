5dfeb02a08a23cb6f6116027965acc44219f2a9d
nicolamassarenti <nicola.massarenti@gmail.com>
2021-11-29 09:12:37 +0100

    Added imports

--- package/src/wiser.gcloud.storage/wiser/gcloud/storage/__init__.py
+++ package/src/wiser.gcloud.storage/wiser/gcloud/storage/__init__.py
@@ -6,3 +6,8 @@ except ImportError:
     import pkgutil
 
     __path__ = pkgutil.extend_path(__path__, __name__)
+
+from wiser.gcloud.storage.services.storage_service import Storage
+from wiser.gcloud.storage.types.storage_location import StorageLocationBuilder
+
+__all__ = ["Storage", "StorageLocationBuilder"]
